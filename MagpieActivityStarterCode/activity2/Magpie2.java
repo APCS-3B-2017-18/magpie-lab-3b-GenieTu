c5936caded804bd43b0681fc712f967560681c50
GenieTu <Genie.Tu@warriorlife.net>
2018-03-15 22:01:37 -0700

    Mpgpie2 changed 10:00pm

--- MagpieActivityStarterCode/activity2/Magpie2.java
+++ MagpieActivityStarterCode/activity2/Magpie2.java
@@ -35,12 +35,37 @@ public class Magpie2
 		{
 			response = "Why so negative?";
 		}
+                else if (statement.trim().length() == 0)
+		{
+			response = "Say something, please.";
+		}
 		else if (statement.indexOf("mother") >= 0
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0)
 		{
 			response = "Tell me more about your family.";
+		}
+		else if (statement.indexOf("cat") >= 0
+                      || statement.indexOf("dog") >= 0)
+		{
+			response = "Tell me more about your pets.";
+		}
+		else if (statement.indexOf("Mrs. Wong") >= 0)
+		{
+			response = "She sounds like a good teacher.";
+		}
+		else if (statement.indexOf("Why") >= 0)
+		{
+			response = "Beats me.";
+		}
+		else if (statement.indexOf("hungry") >= 0)
+		{
+			response = "Go and eat something.";
+		}
+		else if (statement.indexOf("Who") >= 0)
+		{
+			response = "It's a secret.";
 		}
 		else
 		{
@@ -55,12 +80,12 @@ public class Magpie2
 	 */
 	private String getRandomResponse()
 	{
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
 		String response = "";
 		
-		if (whichResponse == 0)
+                if (whichResponse == 0)
 		{
 			response = "Interesting, tell me more.";
 		}
@@ -75,6 +100,14 @@ public class Magpie2
 		else if (whichResponse == 3)
 		{
 			response = "You don't say.";
+		}
+		else if (whichResponse == 4)
+		{
+			response = "I wonder why.";
+		}
+		else if (whichResponse == 5)
+		{
+			response = "Me too.";
 		}
 
 		return response;
